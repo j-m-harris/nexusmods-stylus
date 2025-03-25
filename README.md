481b62ae6eb2e62aeaaa524533f3765fc9805015
John Harris <john.harris@nexusmods.com>
2025-03-25 18:19:10 +0000

    feat: mods grid list view

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # nexusmods-stylus
+
+Just for fun, some [Stylus](https://chromewebstore.google.com/detail/stylus/clngdbkpkpeebahjckkjfobafhncgmne) themes for [NexusMods](https://www.nexusmods.com).
---
+++ mods-grid-list-view.js
@@ -0,0 +1,49 @@
+/* ==UserStyle==
+@name           www.nexusmods.com
+@namespace      github.com/openstyles/stylus
+@version        1.0.0
+@description    Style the mods grid into a list view.
+@author         jmharris
+==/UserStyle== */
+@-moz-document url-prefix("https://www.nexusmods.com") {
+    div.mods-grid {
+        grid-template-columns: none;
+        row-gap: 1.5rem;
+
+        div.group\/mod-tile {
+            min-height: 217px;
+
+            > div.relative {
+                width: 385px;
+                position: absolute;
+            }
+
+            > div.px-3.pb-5.pt-3,
+            > div.min-h-8.px-3 {
+                padding-left: 400px;
+
+                > div.divide-y > div.flex {
+                    display: inline-block;
+                    vertical-align: top;
+                    padding-right: 1.5rem;
+
+
+
+                    > a {
+                        vertical-align: top;
+                    }
+                    > p.text-neutral-subdued {
+                        vertical-align: top;
+                        display: inline-block;
+                        padding-right: 1.5rem;
+
+                        > svg {
+                            display: inline-block;
+                            margin-right: 3px;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
